8ec360c8330159065253f1af99d4484ad09b336e
kemichai <konstantinos.michailos@gmail.com>
2022-01-20 17:30:26 +0100

    slowly looking at migration codes

--- rfmpy/original_codes/05_Ivrea.Obs.Migration.py
+++ rfmpy/original_codes/05_Ivrea.Obs.Migration.py
@@ -26,67 +26,36 @@ ori_prof = 90
 ####################
 # READ AND PROJECT #
 ####################
-
 # Read stations and project
-
-sta, dxSta, dySta = tools.Read_Seismic_Stations(
-    file=path + "Seismic/IA_StCoordinates.txt", ori_prof=ori_prof
-)
+sta, dxSta, dySta = tools.Read_Seismic_Stations(file=path + "Seismic/IA_StCoordinates.txt", ori_prof=ori_prof)
 
 lon_c = sta["LONSTA"].mean()  # Center of the profile
 lat_c = sta["LATSTA"].mean()  # Center of the profile
 
 # Read RFs
-
-stream = tools.Read_Traces(
-    RF_list=path + "Seismic/" + rf_list, sta=sta, ori_prof=ori_prof
-)
+stream = tools.Read_Traces(RF_list=path + "Seismic/" + rf_list, sta=sta, ori_prof=ori_prof)
 
 ########################
 # MIGRATION PARAMETERS #
 ########################
-
 migration_params = setup.Migration_Params(dx=dxSta, dy=dySta)
-
 print(migration_params)
-
 minx, maxx, pasx = migration_params[:3]
 miny, maxy, pasy = migration_params[3:6]
 minz, maxz, pasz = migration_params[6:9]
 inc, zmax = migration_params[9:11]
-
 print(minx, maxx)
 print(miny, maxy)
 print(minz, maxz)
-
 #######################
 # PERFORM RAY TRACING #
 #######################
-
 # With a 2D velocity model
-
-# stream = tools.tracing_2D(
-#     stream=stream,
-#     ori_prof=ori_prof,
-#     path_velocity_model=path_velocity_model,
-#     parameters=migration_params,
-#     lon_c=lon_c,
-#     lat_c=lat_c,
-#     dx=dxSta,
-#     dy=dySta,
-# )
-
+# stream = tools.tracing_2D(stream=stream, ori_prof=ori_prof, path_velocity_model=path_velocity_model,
+#                           parameters=migration_params, lon_c=lon_c, lat_c=lat_c, dx=dxSta, dy=dySta,)
 # Or with a 1D velocity model
-
-stream = tools.tracing_1D(
-    stream=stream,
-    ori_prof=ori_prof,
-    parameters=migration_params,
-    lon_c=lon_c,
-    lat_c=lat_c,
-    zMoho=50,
-)
-
+stream = tools.tracing_1D(stream=stream, ori_prof=ori_prof,parameters=migration_params,
+                          lon_c=lon_c, lat_c=lat_c, zMoho=50,)
 ###################################
 # PERFORM TIME-TO-DEPTH MIGRATION #
 ###################################
@@ -102,26 +71,16 @@ stream = tools.tracing_1D(
     so that you select everything that falls into bazmean +- dbaz
     bazmean == 180 and dbaz == 180 means you select everything (360 deg interval)"""
 
-mObs = tools.ccpM(
-    stream, migration_params, sta, phase="PS", stack=0, dbaz=180, bazmean=180
-)
+mObs = tools.ccpM(stream, migration_params, sta, phase="PS", stack=0, dbaz=180, bazmean=180)
 
 #############
 # Smoothing #
 #############
-
 mObs = tools.ccp_smooth(mObs, migration_params)
 mObs[np.abs(mObs) < np.max(np.abs(mObs)) * 15 / 100] = 0
 mObs = tools.ccpFilter(mObs)
-
 ############
 # Plotting #
 ############
-
-pt.Migration(
-    Gp=mObs,
-    parameters=migration_params,
-    sta=sta,
-    filename=False
-    # filename="/Users/mscarpon/Desktop/MigrationTest.png",
-)
+pt.Migration(Gp=mObs, parameters=migration_params, sta=sta, filename=False)
+             # filename="/Users/mscarpon/Desktop/MigrationTest.png",
