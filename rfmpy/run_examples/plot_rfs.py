5f3866d7de95d17d259225daabb384e4050670f8
kemichai <konstantinos.michailos@gmail.com>
2022-01-24 15:05:16 +0100

    working on the plotting functions

--- rfmpy/run_examples/plot_rfs.py
+++ rfmpy/run_examples/plot_rfs.py
@@ -1,3 +1,11 @@
+"""
+Code for plotting receiver functions.
+
+Original code written by Matteo Scarponi on 30.11.2021
+Location: Chavannes-pres-renens, CH
+Date: Jan 2022
+Author: Konstantinos Michailos
+"""
 import glob
 import obspy
 import numpy as np
@@ -5,90 +13,75 @@ from rfmpy.visuals import plotting as plt_rf
 from rfmpy.visuals import tools
 import matplotlib.pyplot as plt
 from obspy.taup import TauPyModel
+import platform
+from obspy.geodetics import kilometers2degrees
+
 
 ###########################################
 # Define list of stations for reading RFs #
 ###########################################
-
 # TODO: turn this into a function
 # TODO: Why plot more than one station at the same time?
+# TODO: Write a code for the pyrft functions as well
 stations = ["VDL", 'AIGLE', "DAVOX"]  # !!! Based on number of stations check out...
 # ... the number of subplots you want to have: LINE 139
-#############################
-# Path where RFs are stored #
-#############################
 
+# Set up parameters and paths
+if platform.node().startswith('kmichailos-laptop'):
+    data_root_dir = '/media/kmichailos/SEISMIC_DATA/Data_archive'
+    codes_root_dir = '/home/kmichailos/Desktop/codes/bitbucket'
+    desktop_dir = '/home/kmichailos/Desktop'
+    hard_drive_dir = '/media/kmichailos/SEISMIC_DATA/'
+else:
+    data_root_dir = '/media/kmichall/SEISMIC_DATA/Data_archive'
+    codes_root_dir = '/home/kmichall/Desktop/Codes/bitbucket'
+    desktop_dir = '/home/kmichall/Desktop'
+    hard_drive_dir = '/media/kmichall/SEISMIC_DATA/'
+
+# Path at which receiver functions are stored
 pathRF = "/home/kmichall/Desktop/RF_test/RF_1/"
 
-##########################################################################
-# Compute a reference ray parameter (pref) for normal moveout correction #
-##########################################################################
-
+# Compute a reference ray parameter (pref) for normal moveout correction
 model = TauPyModel(model="iasp91")
 km_to_deg = 111.19
-pref = (model.get_travel_times(source_depth_in_km=0, distance_in_degree=65,
-                               phase_list=["P"])[0].ray_param_sec_degree/ km_to_deg)
-##############################################
-# Plotting and moveout correction parameters #
-##############################################
-
+pref = kilometers2degrees((model.get_travel_times(source_depth_in_km=0,
+                                                  distance_in_degree=65,
+                                                  phase_list=["P"])[0].ray_param_sec_degree))
+# Plotting and moveout correction parameters
 bazstart = 10
 bazend = 370
 bazstep = 10
 amplitude = 4.5
 
 Z, VP, VS = plt_rf.get_iasp91(zmax=200, step=0.25, zmoho=75)
-##########
-##########
-# FIGURE #
-##########
-##########
+
 
 f = plt.figure(1, figsize=[8, 6])
-
-################################
-# Loop on stations to read RFs #
-################################
-
+# Loop on stations to read RFs
 station_number = 0
 for station in stations:
     station_number += 1
     files = glob.glob(pathRF + "*" + station + "*")
     if len(files) == 0:
         continue
-
     stream = obspy.read(pathRF + "*" + station + "*")
-
     for trace in stream:
-
-        #############################################################
-        # Compute ray parameter for the single RF in SECONDS PER KM #
-        #############################################################
-
+        # Compute ray parameter for the single RF in SECONDS PER KM
         trace.prai = (model.get_travel_times(source_depth_in_km=trace.stats.sac.evdp / 1000,
                                              distance_in_degree=trace.stats.sac.dist,
-                                             phase_list=["P"],)[0].ray_param_sec_degree/ km_to_deg)  # SECONDS PER KM
+                                             phase_list=["P"],)[0].ray_param_sec_degree/ km_to_deg)  # SECONDS/KM
         trace.stats.baz = trace.stats.sac.baz
-        trace.stats.sac.a = 5
+        # trace.stats.sac.a = 5
 
-        ###################################
-        # Apply normal moveout correction #
-        ###################################
-
+        # Apply normal moveout correction
         # This step is very useful before stacking RFs in time
         # as it homogenizes the RF delay times, accounting for different earthquake sources
         # which happened at different depths and distances with respect to the seismic station
-
         # You can compare the effect of with/without this correction by commenting line 99
-
         trace.data = tools.rfmops(trace, pref, Z, VP, VS)
-
     stream.sort(keys=["baz"])
 
-    #############################################################################
-    # Prepare to plot RFs, STACKED by back-azimuthal direction for each station #
-    #############################################################################
-
+    # Prepare to plot RFs, STACKED by back-azimuthal direction for each station
     allbaz = np.arange(bazstart, bazend, bazstep)
     stack = np.zeros((len(allbaz), len(stream[0].data)))
     count = np.zeros(len(allbaz))
@@ -111,10 +104,7 @@ for station in stations:
             stack[baz, :] = np.nan
         ibaz.append(np.squeeze(indexes))
 
-    #############################################################################################
     # Prepare subplots - > number of subplots has to be adjusted based on number of stations!!! #
-    #############################################################################################
-
     # Watch out for paramter trace.stats.sac.a:
     # it stores the delay of the direct-P arrival with respect to the
     # beginning of the trace.
@@ -123,11 +113,9 @@ for station in stations:
 
     ax = plt.subplot(1, len(stations), station_number)
     tt = range(len(stream[0].data)) * trace.stats.sac.delta - trace.stats.sac.a
-
     # Loop on the back-azimuths and plot the stacked traces for the given interval
     for i in range(len(allbaz)):
         ax.plot(tt, stack[i, :] * amplitude + i, "k", lw=0.5, zorder=-i)
-
         ax.fill_between(tt, y1=stack[i, :] * amplitude + i, y2=i,
                         where=[stack[i, j] * 10 + i > i for j in range(len(stream[0].data))],
                         color="dodgerblue", zorder=-i,)
@@ -151,9 +139,7 @@ for station in stations:
         ax.set_yticklabels([])
     ax.set_xlabel("Time (s)")
 
-    ################################################
     # WRITING NUMBER OF TRACES ON THE RIGHT y-AXIS #
-    ################################################
     ax = ax.twinx()  # instantiate a second axes that shares the same x-axis
     color = "tab:blue"
     ax.set_ylim([-1, len(allbaz) + 1])
--- rfmpy/run_examples/temporary_file.py
+++ rfmpy/run_examples/temporary_file.py
@@ -1,4 +1,68 @@
 """
+Create list of seismic sites used.
+Plot seismic network.
+
+Location: Chavannes-pres-renens, CH
+Date: Jan 2022
+Author: Konstantinos Michailos
+"""
+import rfmpy.utils.RF_Util as rf_util
+import platform
+
+# Set up parameters and paths
+if platform.node().startswith('kmichailos-laptop'):
+    data_root_dir = '/media/kmichailos/SEISMIC_DATA/Data_archive'
+    codes_root_dir = '/home/kmichailos/Desktop/codes/bitbucket'
+    desktop_dir = '/home/kmichailos/Desktop'
+    hard_drive_dir = '/media/kmichailos/SEISMIC_DATA/'
+else:
+    data_root_dir = '/media/kmichall/SEISMIC_DATA/Data_archive'
+    codes_root_dir = '/home/kmichall/Desktop/Codes/bitbucket'
+    desktop_dir = '/home/kmichall/Desktop'
+    hard_drive_dir = '/media/kmichall/SEISMIC_DATA/'
+
+# DATA_RFAA_part_1
+path_wavs_list_part1 = [hard_drive_dir + 'RF_data/DATA_RFAA_part_1/SWISS/data/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_1/EASI/easi_data/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_1/FRANCE/data_sort/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_1/North_Italy/events_fri_ven/']
+
+path_wavs_list_part2 = [hard_drive_dir + 'RF_data/DATA_RFAA_part_2/Austria/data_AAA_corrected/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_2/CIFAlps/data_YP2012/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_2/data_DINAR/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_2/HU_SK/data/']
+# DATA_RFAA_part_3
+path_wavs_list_part3 = [hard_drive_dir + 'RF_data/DATA_RFAA_part_3/AARF/DATA_MOBST/data/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_3/AARF/DATA_PERMST/data/',
+                        hard_drive_dir + 'RF_data/DATA_RFAA_part_3/GERMANY/DE_AA_RF/DATA/data/']
+path_wavs_list_part4 = [hard_drive_dir + 'RF_data/CIFALPS/data_YP2012/']
+# INGV
+path_wavs_list_part5 = [hard_drive_dir + 'RF_data/INGV-Permanent-data/',
+                        hard_drive_dir + 'RF_data/INGV-Temporary-data/data/']
+# List of unique seismic sites
+sta1 = rf_util.get_station_info(path_wavs_list_part1)
+sta2 = rf_util.get_station_info(path_wavs_list_part2)
+sta3 = rf_util.get_station_info(path_wavs_list_part3)
+sta4 = rf_util.get_station_info(path_wavs_list_part4)
+sta5 = rf_util.get_station_info(path_wavs_list_part5)
+
+
+sta = sta1 + sta2 + sta3 + sta4 + sta5
+
+unique_all_sta = []
+for s in sta:
+    if s not in unique_all_sta:
+        unique_all_sta.append(s)
+# using this for making the gmt plot
+
+# For plotting see
+# wiggle_bins functions in miscellaneous
+
+
+
+
+
+"""
 Download metadata information to doublecheck the horizontal components are oriented to
 North and East. Will need to check this before I apply the rotations.
 
