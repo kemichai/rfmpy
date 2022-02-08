28f850007157ca0918a6875bab22f365ef96c4a8
kemichai <konstantinos.michailos@gmail.com>
2022-02-08 16:46:53 +0100

    moved things around, added quakeml cat

--- rfmpy/miscellaneous/data_used_info.py
+++ rfmpy/miscellaneous/data_used_info.py
@@ -1,6 +1,7 @@
 """
 Number of traces...
 """
+import platform
 import glob
 # Set up parameters and paths
 if platform.node().startswith('kmichailos-laptop'):
@@ -27,7 +28,7 @@ path_wavs_list_part1 = [
                         hard_drive_dir + 'RF_data/DATA_RFAA_part_3/AARF/DATA_MOBST/data/',
                         hard_drive_dir + 'RF_data/DATA_RFAA_part_3/AARF/DATA_PERMST/data/',
                         hard_drive_dir + 'RF_data/DATA_RFAA_part_3/GERMANY/DE_AA_RF/DATA/data/',
-                        hard_drive_dir + 'RF_data/CIFALPS/data_YP2012/',
+                        # hard_drive_dir + 'RF_data/CIFALPS/data_YP2012/',
                         hard_drive_dir + 'RF_data/INGV-Permanent-data/',
                         hard_drive_dir + 'RF_data/INGV-Temporary-data/data/',
 ]
@@ -40,6 +41,8 @@ for path in path_wavs_list_part1:
         if ev_name not in unigue_events:
             unigue_events.append(ev_name)
 unigue_events.sort()
+print('Number of tele-events used: ', str(len(unigue_events)))
+
 
 unique_traces = []
 for path in path_wavs_list_part1:
@@ -80,3 +83,46 @@ for net in inv:
             cha_name = net.code + '.' + sta.code + '.' + cha.code
             if e_trace_name == cha_name:
                 print(cha_name, cha.azimuth, cha.dip)
+
+
+# CREATE a quakeml catalog for the teleseismic events... from the cut waveforms...
+from obspy import UTCDateTime, read
+from obspy.core.event import Catalog, Event, Origin, Magnitude
+import obspy
+import numpy as np
+cat = Catalog()
+cat.description = "Teleseismic events for RF calculations with AlpArray data"
+unigue_events = []
+for path in path_wavs_list_part1:
+    all_event_dir = glob.glob(path + 'P*')
+    for event_dir in all_event_dir:
+        ev_name = event_dir.split('/')[-1]
+        print(ev_name)
+        if ev_name not in unigue_events:
+            unigue_events.append(ev_name)
+            traces = glob.glob(event_dir + '/*SAC')
+            trace = read(traces[0])
+            tr = trace[0]
+            e = Event()
+            o = Origin()
+            m = Magnitude()
+
+            o.time = tr.stats.starttime + 120
+            o.latitude = tr.stats.sac.evla
+            o.longitude = tr.stats.sac.evlo
+            o.depth = tr.stats.sac.evdp * 1000
+            e.comments.append(obspy.core.event.Comment(text="%s" % ev_name))
+            m.mag = tr.stats.sac.mag
+            m.magnitude_type = "M"
+
+            e.origins = [o]
+            e.magnitudes = [m]
+            m.origin_id = o.resource_id
+
+            cat.append(e)
+
+cat.events.sort(key=lambda e: e.origins[-1].time)
+cat.plot('global')
+unigue_events.sort()
+print('Number of tele-events used: ', str(len(unigue_events)))
+cat.write('Teleseismic_events_RF.xml', format='QUAKEML')
