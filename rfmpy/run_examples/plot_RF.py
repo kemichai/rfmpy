a43fadd8b711caa0ea95dd2dd51fa295081d5bc2
kemichai <konstantinos.michailos@gmail.com>
2022-03-17 18:03:45 +0100

    working on the RF plots vs BAZ

--- rfmpy/run_examples/plot_RF.py
+++ rfmpy/run_examples/plot_RF.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from obspy.taup import TauPyModel
 import platform
 from obspy.geodetics import kilometers2degrees
+import matplotlib
 
 
 ###########################################
@@ -23,7 +24,7 @@ from obspy.geodetics import kilometers2degrees
 # TODO: turn this into a function
 # TODO: Why plot more than one station at the same time?
 # TODO: Write a code for the pyrft functions as well
-stations = ["PANIX","AIGLE",]# "ROTHE","SIMPL", "LAUCH"]  # !!! Based on number of stations check out...
+stations = ["PANIX"]# "ROTHE","SIMPL", "LAUCH"]  # !!! Based on number of stations check out...
 # ... the number of subplots you want to have: LINE 139
 
 # Set up parameters and paths
@@ -37,6 +38,17 @@ else:
     codes_root_dir = '/home/kmichall/Desktop/Codes/bitbucket'
     desktop_dir = '/home/kmichall/Desktop'
     hard_drive_dir = '/media/kmichall/SEISMIC_DATA/'
+
+# Set figure details
+font = {'family': 'normal',
+        'weight': 'normal',
+        'size': 18}
+matplotlib.rc('font', **font)
+# Set figure width to 12 and height to 9
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 7
+fig_size[1] = 10
+plt.rcParams["figure.figsize"] = fig_size
 
 # Path at which receiver functions are stored
 # pathRF = "/home/kmichall/Desktop/RF_test/RF_1/"
@@ -57,7 +69,7 @@ amplitude = 5
 Z, VP, VS = plt_rf.get_iasp91(zmax=200, step=0.25, zmoho=75)
 
 
-f = plt.figure(1, figsize=[8, 6])
+f = plt.figure(1)
 # Loop on stations to read RFs
 station_number = 0
 for station in stations:
@@ -126,11 +138,19 @@ for station in stations:
     ax.axvline(x=0, ymin=0, ymax=1, color="g", alpha=0.5, linestyle="--", lw=1, zorder=-20)
     # ax.axvline(x=1, ymin=0, ymax=1, color="r", alpha=0.25, linestyle="--", lw=1, zorder=-20)
     # ax.axvline(x=2, ymin=0, ymax=1, color="r", alpha=0.25, linestyle="--", lw=1, zorder=-20)
+    print(-i)
 
-    ax.grid(True, alpha=0.5, lw=0.4, linestyle="--")
-    ax.set_xticks([0, 5, 10, 15, 20, 30])
+
+    ax.set_axisbelow(True)
     ax.set_yticks(np.arange(len(allbaz)))
-    ax.set_xlim([-5, 20])
+    major_ticks = np.arange(0, 25, 5)
+    minor_ticks = np.arange(-5, 25, 1)
+    ax.set_xticks(major_ticks)
+    ax.set_xticks(minor_ticks, minor=True)
+    # Or if you want different settings for the grids:
+    ax.grid(which='minor', alpha=0.7, linestyle="--", zorder=0)
+    ax.grid(which='major', alpha=0.9, zorder=0)
+    ax.set_xlim([-5, 25])
     ax.set_ylim([-1, len(allbaz) + 1])
     ax.set_yticklabels(allbaz)
     ax.text(0.975, 0.975, station, transform=ax.transAxes, va="center", ha="right")
@@ -152,5 +172,6 @@ for station in stations:
     ax.yaxis.label.set_color("tab:blue")
 
 plt.tight_layout()
+plt.savefig('/home/kmichall/Desktop/' + station, format='png', dpi=300)
 plt.show()
 plt.close()
