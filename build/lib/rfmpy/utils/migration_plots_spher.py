d49a3ae154124a549f52f8133f73bae7cd0dc0d8
kmichailos <17120684+kemichai@users.noreply.github.com>
2022-04-28 16:52:11 +0200

    re-organised the profile 2d stuff

--- build/lib/rfmpy/utils/migration_plots_spher.py
+++ build/lib/rfmpy/utils/migration_plots_spher.py
@@ -17,6 +17,10 @@ import matplotlib.gridspec as gridspec
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from collections import OrderedDict
 import matplotlib.patches as patches
+from scipy.interpolate import RegularGridInterpolator
+from pyproj import Geod
+import pyproj
+from obspy.geodetics import degrees2kilometers, kilometers2degrees
 
 
 def add_legend(ax, fontsize=12):
@@ -52,7 +56,7 @@ fontsize = 12
 markersize = 100
 
 
-def plot_migration_profile(Gp, migration_param_dict, sta, work_directory, filename=False):
+def plot_migration_profile_old(Gp, migration_param_dict, sta, work_directory, filename=False):
 
 
     # Read migration parameters
@@ -126,6 +130,293 @@ def plot_migration_profile(Gp, migration_param_dict, sta, work_directory, filena
     plt.close()
 
     return
+
+
+def get_end_point(lat1, lon1, baz, d):
+    """
+    Calculates the end point in lon, lat given we know:
+     1) the initial point,
+     2) the distance and
+     3) the back azimuth
+    """
+    # Radius of the Earth
+    R = 6371
+    # Convert degrees to radians
+    baz_ = np.radians(baz)
+    # Current lat point converted to radians
+    lat1 = np.radians(lat1)
+    # Current long point converted to radians
+    lon1 = np.radians(lon1)
+
+    lat2 = np.arcsin(np.sin(lat1) * np.cos(d / R) + np.cos(lat1) * np.sin(d / R) * np.cos(baz_))
+    lon2 = lon1 + np.arctan2(np.sin(baz_) * np.sin(d / R) * np.cos(lat1),
+                             np.cos(d / R) - np.sin(lat1) * np.sin(lat2))
+    # Convert to degrees
+    lat2 = np.degrees(lat2)
+    lon2 = np.degrees(lon2)
+    return lat2, lon2
+
+
+def az2baz(angle):
+    if angle >= 180.:
+        baz = angle - 180.
+    else:
+        baz = angle + 180.
+    return baz
+
+
+def get_perpendicular_azimuth(az):
+    if az > 270:
+        perp_az1 = (az + 90) - 360
+        perp_az2 = az - 90
+    elif az < 90:
+        perp_az1 = az + 90
+        perp_az2 = az - 90 + 360
+    else:
+        perp_az1 = az + 90
+        perp_az2 = az - 90
+    return perp_az1, perp_az2
+
+
+# TODO: finish documentation...
+def project(station_lats, station_lons, point_lat, point_lon, angle):
+    """
+    Projects stations coordinates to a given point (lon, lat) in respect to an angle to the north.
+
+    NOTE: Takes station coordinates and projects them with respect to the center of the profile and the angle
+          of the profile with respect to the North direction.
+          Output is in [km] for x,y coordinates with respect to lono and lato
+
+    :type station_lats:
+    :param station_lats:
+    :type station_lons:
+    :param station_lons:
+    :type point_lat:
+    :param point_lat:
+    :type point_lon:
+    :param point_lon:
+    :type angle:
+    :param angle:
+
+    :returns:
+    """
+
+    ylat = (station_lats - point_lat) * 111.19
+    xlon = (station_lons - point_lon) * 111.19 * np.cos(np.radians(station_lats))
+
+    M = np.array([[np.cos(np.radians(angle)), np.sin(np.radians(angle))],
+                  [-np.sin(np.radians(angle)), np.cos(np.radians(angle))],])
+    R = np.dot(np.column_stack((xlon, ylat)), M)
+
+    distx = R[:, 1]
+    disty = R[:, 0]
+
+    return distx, disty
+
+
+def project_stations(sta, ori_prof, point_lat, point_lon):
+    """
+    # TODO: finish documentation...
+
+    :type sta: Pandas DataFrames.
+    :param sta: Station details.
+    :type ori_prof:
+    :param ori_prof:
+    :type point_lat:
+    :param point_lat:
+    :type point_lon:
+    :param point_lon:
+
+    :return:
+    """
+
+    xsta, ysta = project(sta["LATSTA"].values, sta["LONSTA"].values, point_lat, point_lon, ori_prof)
+
+    dx, dy = 0, 0
+    sta["XSTA"] = xsta + dx
+    sta["YSTA"] = ysta + dy
+    # Set elevation with negative numbers in km
+    sta["ZSTA"] = (-1) * sta["ALTSTA"].values / 1000
+
+    return sta, dx, dy
+
+
+def create_2d_profile(G3, migration_param_dict, profile_points, sta, swath=200, plot=True):
+    """
+
+    :param G3:
+    :param migration_param_dict:
+    :param profile_points:
+    :param sta:
+    :param swath:
+    :param plot:
+    :return:
+    """
+
+    # Read migration parameters
+    minx = migration_param_dict['minx']
+    maxx = migration_param_dict['maxx']
+    pasx = migration_param_dict['pasx']
+    miny = migration_param_dict['miny']
+    maxy = migration_param_dict['maxy']
+    pasy = migration_param_dict['pasy']
+    minz = migration_param_dict['minz']
+    maxz = migration_param_dict['maxz']
+    pasz = migration_param_dict['pasz']
+
+    # Define grid
+    grid_3d_x = np.arange(minx, maxx + pasx, pasx)
+    grid_3d_y = np.arange(miny, maxy + pasy, pasy)
+    grid_3d_z = np.arange(minz, maxz + pasz, pasz)
+
+    # Interpolate in 3D the RF amplitudes
+    G_interpolated = RegularGridInterpolator((grid_3d_x, grid_3d_y, grid_3d_z), G3)
+
+    # Profile start and end
+    lon0, lat0 = profile_points[0][0], profile_points[0][1]
+    lon1, lat1 = profile_points[1][0], profile_points[1][1]
+
+    # Profile swath (km)?????
+    profile_swath = swath
+    # Profile azimuth
+    geoid = pyproj.Geod(ellps='WGS84')
+    profile_az, back_azimuth, profile_len_ = geoid.inv(lon0, lat0, lon1, lat1)
+    # Profile length (km)
+    profile_len = profile_len_ / 1000
+    # Two perpendicular azimuths
+    az1, az2 = get_perpendicular_azimuth(profile_az)
+
+    # By doing this we have roughly the same spacing as the grid
+    num_of_points = int(round(profile_len/degrees2kilometers(pasx))) - 1
+    print(num_of_points, profile_len)
+
+    # Coordinates of the points along the profile knowing start and end of profile
+    n_extra_points = num_of_points + 100 # number of these points
+    geoid = Geod(ellps="WGS84")
+    extra_points = np.array(geoid.npts(lon0, lat0, lon1, lat1, n_extra_points))
+    # Create new lists of lon, lat, dep and amps (interpolated)
+    lon_points_along_prof = extra_points[:, 0]
+    lat_points_along_prof = extra_points[:, 1]
+
+    # For each point of the profile find the two end point perpendicular
+    # to them in a distance equal to the swath of the profile
+    amps = []
+    for i, lon in enumerate(lon_points_along_prof):
+        # Two points perpendicular to the azimuth of the profile at each point of the profile
+        lat_1, lon_1 = get_end_point(lat_points_along_prof[i], lon_points_along_prof[i], az1, profile_swath)
+        lat_2, lon_2 = get_end_point(lat_points_along_prof[i], lon_points_along_prof[i], az2, profile_swath)
+        n_extra_points_ = 20  # number of these points
+        points_perpendicular_2_prof = np.array(geoid.npts(lon_1, lat_1, lon_2, lat_2, n_extra_points_))
+
+        temp_lon = points_perpendicular_2_prof[:, 0]
+        temp_lat = points_perpendicular_2_prof[:, 1]
+        # interpolate for each of the 5 points perpendicular to the profile
+        amps_matrix_temp = np.zeros((len(grid_3d_z)))
+        nG = np.zeros((len(grid_3d_z)))
+
+        for j, lon_ in enumerate(temp_lon):
+            # print(j)
+            amps_temp = np.zeros((len(grid_3d_z)))
+            for k, z in enumerate(grid_3d_z):
+                # print(z)
+                point = np.array([lon_, temp_lat[j], z])
+                VPinterp = G_interpolated(point)
+                amps_temp[k] = VPinterp[0]
+                # print(VPinterp[0])
+            amps_matrix_temp = amps_matrix_temp + amps_temp
+            nG = nG + 1
+        # add and divide by the number of stacks
+        G = np.divide(amps_matrix_temp, nG)
+        amps.append(G.tolist())
+    G2 = np.array(amps)  # 2 dimensions
+
+    sta, dxSta, dySta = project_stations(sta=sta, ori_prof=profile_az,
+                                         point_lat=lat0, point_lon=lon0)
+    if plot:
+        # Plot stations and profile
+        lons = [lon0, lon1]
+        lats = [lat0, lat1]
+        plt.plot(lons, lats, c='dodgerblue')
+        plt.scatter(lon0, lat0, c='dodgerblue', marker='s', edgecolor='k', s=50)
+        plt.scatter(lon1, lat1, c='dodgerblue', marker='o', edgecolor='k', s=50)
+        plt.scatter(sta["LONSTA"], sta["LATSTA"],
+                    c='r', marker='v', edgecolor='k', s=100)
+        plt.ylim(miny, maxy)
+        plt.xlim(minx, maxx)
+        plt.show()
+
+    # Grid preparation
+    xx = np.arange(0, profile_len, profile_len / n_extra_points)
+    zz = np.arange(minz, maxz + pasz, pasz)
+
+    return G2, sta, xx, zz
+
+
+def plot_migration_profile(Gp, xx, zz, migration_param_dict, sta, work_directory, filename=False):
+
+    XX, ZZ = np.meshgrid(xx, zz)
+
+    # Todo: figure out how to plot depths here...
+    # zz_corr = []
+    # R = 6371.009
+    # for i, dist in enumerate(prof_dist):
+    #     correction = np.sqrt(dist**2 + R)
+    #     print(correction)
+    #     zz_corr = zz + correction
+
+
+
+    # COLOR PALETTE AND COLORMAP
+    # cork, bam, broc, vik
+    pal_col = work_directory + "/data/colormaps/vik.txt"
+    pal_col = pd.read_csv(pal_col, header=None, index_col=False, sep="\s+", names=["R", "G", "B"])
+    cm = LinearSegmentedColormap.from_list("blue2red", pal_col.values, len(pal_col))
+    c = np.min([np.max(Gp), 0.1])
+    c = 0.06
+    CL = 2
+
+    # PLOT
+    f = plt.figure(1, figsize=[10, 8])
+    gs0 = gridspec.GridSpec(nrows=1, ncols=1, figure=f,
+                            hspace=0.08, right=0.91, left=0.09, bottom=0.08, top=0.96,)
+
+    ax = f.add_subplot(gs0[0])  # Ray tracing
+    # bwr, seismic, coolwarm, RdBu
+    m = ax.pcolormesh(XX, ZZ, Gp.T, cmap=cm, vmin=-c / CL, vmax=c / CL,
+                      zorder=1, shading="auto")
+    add_colorbar(ax, m)
+    ax.scatter(sta["XSTA"].values, sta["ZSTA"].values,
+               markersize, facecolors="grey", edgecolors="k",
+               marker="v", lw=0.95, zorder=3, clip_on=False,
+               label="Seismic stations", )
+
+    ax.set_aspect("equal")
+    ax.set_xlabel("x [km]", fontsize=fontsize)
+    ax.set_ylabel("z [km]", fontsize=fontsize)
+
+    majorLocator = MultipleLocator(10)
+    minorLocator = MultipleLocator(2.5)
+    ax.xaxis.set_major_locator(majorLocator)
+    ax.xaxis.set_minor_locator(minorLocator)
+    # ax.set_xticks(np.arange(0, 140, 10))
+
+    majorLocator = MultipleLocator(10)
+    minorLocator = MultipleLocator(2.5)
+    ax.yaxis.set_major_locator(majorLocator)
+    ax.yaxis.set_minor_locator(minorLocator)
+    ax.set_yticks(np.arange(10, zz[-1], 10))
+    ax.set_ylim([100, 0])
+
+    ax.tick_params(axis="both", which="major", labelsize=fontsize)
+    ax.tick_params(axis="both", which="minor", labelsize=fontsize)
+    if filename:
+        f.savefig(filename, dpi=200)
+
+    plt.show()
+    plt.close()
+
+    return
+
 
 
 def plot_ray_tracing(st):
--- rfmpy/run_examples/plot_migration_2D_profile.py
+++ rfmpy/run_examples/plot_migration_2D_profile.py
@@ -7,71 +7,8 @@ Author: Konstantinos Michailos
 import rfmpy.core.migration_sphr as rf_mig
 import rfmpy.utils.migration_plots_spher as plot_migration_sphr
 import numpy as np
-import platform
 import os
 import matplotlib.pyplot as plt
-from scipy.interpolate import RegularGridInterpolator
-from pyproj import Geod
-import pyproj
-
-
-# Set up paths
-if platform.node().startswith('kmichailos-laptop'):
-    data_root_dir = '/media/kmichailos/SEISMIC_DATA/Data_archive'
-    codes_root_dir = '/home/kmichailos/Desktop/codes/github'
-    desktop_dir = '/home/kmichailos/Desktop'
-    hard_drive_dir = '/media/kmichailos/SEISMIC_DATA/'
-else:
-    data_root_dir = '/media/kmichall/SEISMIC_DATA/Data_archive'
-    codes_root_dir = '/github'
-    desktop_dir = '/home/kmichall/Desktop'
-    hard_drive_dir = '/media/kmichall/SEISMIC_DATA/'
-
-
-
-def get_end_point(lat1, lon1, baz, d):
-    """
-    Calculates the end point in lon, lat given we know:
-     1) the initial point,
-     2) the distance and
-     3) the back azimuth
-    """
-    # Radius of the Earth
-    R = 6371
-    # Convert degrees to radians
-    baz_ = np.radians(baz)
-    # Current lat point converted to radians
-    lat1 = np.radians(lat1)
-    # Current long point converted to radians
-    lon1 = np.radians(lon1)
-
-    lat2 = np.arcsin(np.sin(lat1) * np.cos(d / R) + np.cos(lat1) * np.sin(d / R) * np.cos(baz_))
-    lon2 = lon1 + np.arctan2(np.sin(baz_) * np.sin(d / R) * np.cos(lat1),
-                             np.cos(d / R) - np.sin(lat1) * np.sin(lat2))
-    # Convert to degrees
-    lat2 = np.degrees(lat2)
-    lon2 = np.degrees(lon2)
-    return lat2, lon2
-
-def az2baz(angle):
-    if angle >= 180.:
-        baz = angle - 180.
-    else:
-        baz = angle + 180.
-    return baz
-
-def get_perpendicular_azimuth(az):
-    if az > 270:
-        perp_az1 = (az + 90) - 360
-        perp_az2 = az - 90
-    elif az < 90:
-        perp_az1 = az + 90
-        perp_az2 = az - 90 + 360
-    else:
-        perp_az1 = az + 90
-        perp_az2 = az - 90
-    return perp_az1, perp_az2
-
 
 
 # Define paths
@@ -99,170 +36,21 @@ m_params = {'minx': minx, 'maxx': maxx,
             'minz': minz, 'maxz': maxz, 'pasz': pasz, 'inc': inc, 'zmax': zmax}
 
 #############################################
-
 # Read the 3D numpy array of the RF amplitudes
 with open('obs_amplitudes_matrix.npy', 'rb') as f:
-    mObs_ = np.load(f)
+    G3_ = np.load(f)
 
-# Define grid
-grid_3d_a = mObs_
-grid_3d_x = np.arange(minx, maxx + pasx, pasx)
-grid_3d_y = np.arange(miny, maxy + pasy, pasy)
-grid_3d_z = np.arange(minz, maxz + pasz, pasz)
+# read stations
+sta = rf_mig.read_stations_from_sac(path2rfs=path)
 
-# Interpolate in 3D
-G_interpolated = RegularGridInterpolator((grid_3d_x, grid_3d_y, grid_3d_z), grid_3d_a)
-pts = np.array([7.4, 46, 6])
-VPinterp = G_interpolated(pts)
+profile_A = np.array([[8, 45.5], [8, 48]])
+G2, sta, xx, zz = plot_migration_sphr.create_2d_profile(G3_, m_params, profile_A, sta,
+                                                        swath=30, plot=True)
 
-# Profile start and end
-ref_pnt = np.array([[grid_3d_x[0], grid_3d_y[1]],
-                    [grid_3d_x[-1], grid_3d_y[-5]]])
-# lon0, lat0 = ref_pnt[0][0], ref_pnt[0][1]
-lon0, lat0 = 8, 45.5
-lon1, lat1 = 8, 48
-# lon1, lat1 = ref_pnt[1][0], ref_pnt[1][1]
-
-# Profile swath (km)?????
-profile_swath = 100
-# Profile azimuth
-geoid = pyproj.Geod(ellps='WGS84')
-profile_az, back_azimuth, profile_len_ = geoid.inv(lon0, lat0, lon1, lat1)
-# Profile length (km)
-profile_len = profile_len_/1000
-# Two perpendicular azimuths
-az1, az2 = get_perpendicular_azimuth(profile_az)
-
-
-# Coordinates of the points along the profile knowing start and end of profile
-n_extra_points = 100 # number of these points
-geoid = Geod(ellps="WGS84")
-extra_points = np.array(geoid.npts(lon0, lat0, lon1, lat1, n_extra_points))
-
-# Create new lists of lon, lat, dep and amps (interpolated)
-lon_points_along_prof = extra_points[:,0]
-lat_points_along_prof = extra_points[:,1]
-
-# For each point of the profile find the two end point perpendicular
-# to them in a distance equal to the swath of the profile
-amps = []
-for i, lon in enumerate(lon_points_along_prof):
-    # Two points perpendicular to the azimuth of the profile at each point of the profile
-    lat_1, lon_1 = get_end_point(lat_points_along_prof[i], lon_points_along_prof[i], az1, profile_swath)
-    lat_2, lon_2 = get_end_point(lat_points_along_prof[i], lon_points_along_prof[i], az2, profile_swath)
-    n_extra_points_ = 5 # number of these points
-    points_perpendicular_2_prof = np.array(geoid.npts(lon_1, lat_1, lon_2, lat_2, n_extra_points_))
-
-    temp_lon = points_perpendicular_2_prof[:,0]
-    temp_lat = points_perpendicular_2_prof[:,1]
-    # interpolate for each of the 5 points perpendicular to the profile
-    amps_matrix_temp = np.zeros((len(grid_3d_z)))
-    nG = np.zeros((len(grid_3d_z)))
-
-    for j, lon_ in enumerate(temp_lon):
-        print(j)
-        amps_temp = np.zeros((len(grid_3d_z)))
-        for k, z in enumerate(grid_3d_z):
-            print(z)
-            point = np.array([lon_, temp_lat[j], z])
-            VPinterp = G_interpolated(point)
-            amps_temp[k] = VPinterp[0]
-            print(VPinterp[0])
-        amps_matrix_temp = amps_matrix_temp + amps_temp
-        nG = nG + 1
-
-    # add and divide by the number of stacks
-    G = np.divide(amps_matrix_temp, nG)
-    amps.append(G.tolist())
-G2 = np.array(amps) # 2 dimensions
 
 mObs = rf_mig.ccp_smooth(G2, m_params)
-mObs[np.abs(mObs) < np.max(np.abs(mObs)) * 15 / 100] = 0
+# mObs[np.abs(mObs) < np.max(np.abs(mObs)) * 15 / 100] = 0
 mObs = rf_mig.ccpFilter(mObs)
-# plot_migration_sphr.plot_migration_profile(Gp=mObs, migration_param_dict=m_params, sta=sta,
-#                                            work_directory=work_dir, filename=False)
-
-Gp = mObs
-migration_param_dict = m_params
-sta = rf_mig.read_stations_from_sac(path2rfs=path)
-import rfmpy.core.migration as rf_mig_
-
-sta, dxSta, dySta = rf_mig_.project_stations(sta=sta, ori_prof=profile_az,
-                                            point_lat=lat0, point_lon=lon0)
-work_directory=work_dir
-filename=False
-
-# Plot stations and profile
-lons = [lon0,lon1]
-lats = [lat0,lat1]
-plt.plot(lons, lats, c='dodgerblue')
-plt.scatter(lon0, lat0, c='dodgerblue', marker='s', edgecolor='k', s=50)
-plt.scatter(lon1, lat1, c='dodgerblue', marker='o', edgecolor='k', s=50)
-plt.scatter(sta["LONSTA"], sta["LATSTA"],
-            c='r', marker='v', edgecolor='k', s=100)
-plt.ylim(miny, maxy)
-plt.xlim(minx, maxx)
-plt.show()
-
-
-xx = np.arange(0, profile_len, profile_len/n_extra_points)
-zz = np.arange(minz, maxz + pasz, pasz)
-
-# Todo: figure out how to plot depths here...
-# zz_corr = []
-# R = 6371.009
-# for i, dist in enumerate(prof_dist):
-#     correction = np.sqrt(dist**2 + R)
-#     print(correction)
-#     zz_corr = zz + correction
-
-
-XX, ZZ = np.meshgrid(xx, zz)
-
-pal_col = work_directory + "/data/colormaps/vik.txt"
-pal_col = pd.read_csv(pal_col, header=None, index_col=False, sep="\s+", names=["R", "G", "B"])
-cm = LinearSegmentedColormap.from_list("blue2red", pal_col.values, len(pal_col))
-c = np.min([np.max(Gp), 0.1])
-c = 0.06
-CL = 2
-
-# PLOT
-f = plt.figure(1, figsize=[10, 8])
-gs0 = gridspec.GridSpec(nrows=1, ncols=1, figure=f,
-                        hspace=0.08, right=0.91, left=0.09, bottom=0.08, top=0.96, )
-
-ax = f.add_subplot(gs0[0])  # Ray tracing
-# bwr, seismic, coolwarm, RdBu
-m = ax.pcolormesh(XX, ZZ, Gp.T, cmap=cm, vmin=-c / CL, vmax=c / CL,
-                  zorder=1, shading="auto")
-add_colorbar(ax, m)
-ax.scatter(sta["XSTA"].values, sta["ZSTA"].values,
-           markersize, facecolors="grey", edgecolors="k",
-           marker="v", lw=0.95, zorder=3, clip_on=False,
-           label="Seismic stations", )
-
-ax.set_aspect("equal")
-ax.set_xlabel("x [km]", fontsize=fontsize)
-ax.set_ylabel("z [km]", fontsize=fontsize)
-
-majorLocator = MultipleLocator(5)
-minorLocator = MultipleLocator(2.5)
-ax.xaxis.set_major_locator(majorLocator)
-ax.xaxis.set_minor_locator(minorLocator)
-ax.set_xticks(np.arange(0, 300, 50))
-
-majorLocator = MultipleLocator(5)
-minorLocator = MultipleLocator(2.5)
-ax.yaxis.set_major_locator(majorLocator)
-# ax.yaxis.set_minor_locator(minorLocator)
-ax.set_yticks(np.arange(0, 100, 20))
-ax.set_ylim([100, 0])
-
-ax.tick_params(axis="both", which="major", labelsize=fontsize)
-ax.tick_params(axis="both", which="minor", labelsize=fontsize)
-if filename:
-    f.savefig(filename, dpi=200)
-
-plt.show()
-plt.close()
+plot_migration_sphr.plot_migration_profile(Gp=mObs, xx=xx, zz=zz, migration_param_dict=m_params, sta=sta,
+                                           work_directory=work_dir, filename=False)
 
