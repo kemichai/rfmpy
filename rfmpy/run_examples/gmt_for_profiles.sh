c0a4b2c027b6d43e9045c76340c3fc8f1d61da8f
kmichailos <17120684+kemichai@users.noreply.github.com>
2022-05-10 16:53:14 +0200

    updated stuff-working with gmt

--- rfmpy/run_examples/gmt_for_profiles.sh
+++ rfmpy/run_examples/gmt_for_profiles.sh
@@ -1,15 +1,17 @@
-gmt begin polar png
+#gmt begin polar png
 gmt set FONT_TITLE 12p,9
 
-gmt subplot begin 4x4 -Fs10c/10c -A+o-0.5c/-0.5c
+#gmt subplot begin 4x4 -Fs10c/10c -A+o-0.5c/-0.5c
 
-gmt basemap -JP?+z -R0/100/30/80 -Baf -B+t'-JP10c+f80 -R0/120/0/50 OR -JP10c+z -R0/120/30/80' -c
-gmt basemap -JP?+z -R0/120/3480/6371 -Baf -B+t'-JP10c+fp -R0/120/0/2891 OR -JP10c+z -R0/120/3480/6371' -c
-awk '{print $1, $2, $3}' xyz.txt| gmt xyz2grd -R0/100/0/100 -I1/5 -Gt.nc
-gmt grdimage t.nc -Baf -B+t -R0/100/0/50 -JP10c+z -png map
+#gmt basemap -JP?+z -R0/100/30/80 -Baf -B+t'-JP10c+f80 -R0/120/0/50 OR -JP10c+z -R0/120/30/80' -c
+#gmt basemap -JP?+z -R0/120/3480/6371 -Baf -B+t'-JP10c+fp -R0/120/0/2891 OR -JP10c+z -R0/120/3480/6371' -c
+#awk '{print $1, $2, $3}' xyz.txt| gmt xyz2grd -R0/100/0/100 -I1/5 -Gt.nc
+awk '{print $1, $2, $3}' xyz2.txt| gmt xyz2grd -R0/3/6320/6370 -I1/25 -Gt.nc
+gmt makecpt -Cmagma -T0/15/1 > seis.cpt
+gmt grdimage t.nc -Baf -B+t -R0/3/6320/6370 -Cseis.cpt -JPa5z -Vl -png map
 #gmt grdimage t.nc -B -JP15c+z6370 -png map
 #gmt grdimage t.nc -B -R0/180/2200/6370 -JP15c+z -png map
 #gmt grdimage t.nc -JP6i+f -Bafg -png map
 #gmt grdimage t.nc -JP6i+f -Bafg -png map --PROJ_ELLIPSOID=sphere
-gmt subplot end
-gmt end show
+#gmt subplot end
+#gmt end show
