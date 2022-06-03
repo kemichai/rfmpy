8eed009416e1bf05bb332415668ea24e81ddeb40
kmichailos <17120684+kemichai@users.noreply.github.com>
2022-06-03 13:03:43 +0200

    update stuff

--- rfmpy/visualisation/gmt/maps/Figure_1/figure_1.sh
+++ rfmpy/visualisation/gmt/maps/Figure_1/figure_1.sh
@@ -17,7 +17,7 @@ gmt set FONT_LABEL Helvetica
 gmt set LABEL_FONT_SIZE 7
 gmt set MAP_FRAME_TYPE plain
 # Directory containing topo grd file
-topodir="/home/kmichall/Desktop/topo"
+topodir="/home/kmichailos/Desktop/topo"
 # Map boundaries
 north=52
 south=41
@@ -29,11 +29,11 @@ proj='-JB10/45/25/45/5i'
 
 # ------------------------------------------------------------------------------------------------------------------- #
 echo Make basemap...
-gmt pscoast -W1/0.05 -Dl $proj -R$west/$east/$south/$north -K -B5WSen -P > $out
+gmt pscoast -W1/0.05 -Dl $proj -R$west/$east/$south/$north -K -B5WSen -P -X1 -Y10 > $out
 # ------------------------------------------------------------------------------------------------------------------- #
 echo Plot topo....
 #gmt grdimage -R -J /home/kmichall/Desktop/topo/topo.0.20.40.55.3sec.grd -CFrance2.cpt -O -K >> $out
-gmt grdimage -R -J /home/kmichall/Desktop/topo/ETOPO1_Bed_g_gmt4.grd -Cmy_topo.cpt -O -K >> $out
+gmt grdimage -R -J $topodir/ETOPO1_Bed_g_gmt4.grd -Cmy_topo.cpt -O -K >> $out
 # ------------------------------------------------------------------------------------------------------------------- #
 gmt pscoast -W1/0.05 -Df -J -R -K -O -P -Sazure1 -N1/0.05p,black -L3.4/49.7/48/200+l+u >> $out
 
@@ -48,9 +48,9 @@ gmt makecpt -Cviridis -T40/110/10  > seis.cpt
 gmt makecpt -Chot -T0/800/100 -D+i -I > seis.cpt
 
 echo Plot scale...
-gmt psscale -Dx1/10.8+o0/0i+w1.2i/0.08i+h+e -R -J -Cmy_topo.cpt -Bx500f250 -Bx+l"Topography (m)" \
+gmt psscale -Dx2.5/10.8+o0/0i+w1.2i/0.08i+h+e -R -J -Cmy_topo.cpt -Bx500f250 -Bx+l"Topography (m)" \
  -O -K --FONT_ANNOT_PRIMARY=8p >> $out
-gmt psscale -Dx1/12.4+o0/0i+w1.2i/0.08i+h+e -R -J  -Cseis.cpt -Bx200f100 -Bx+l"Number of waveforms" \
+gmt psscale -Dx7.0/10.8+o0/0i+w1.2i/0.08i+h+e -R -J  -Cseis.cpt -Bx200f100 -Bx+l"Number of waveforms" \
 -O -K --FONT_ANNOT_PRIMARY=8p >> $out
 
 echo Plot country names...
@@ -72,7 +72,7 @@ echo Plot seismic stations...
 #-O -K -W.5p -Cseis.cpt -t5 >> $out
 #awk '{print $3, $2, $1}' files/rfs_calculated.txt | gmt pstext -R -J -O -K -F+f2p,Helvetica,gray10 -Gwhite >> $out
 
-awk '{print $3, $2, $4}' files/number_of_waveforms.txt | gmt psxy -i0,1,2 -Si.25 -R -J \
+awk '{print $3, $2, $4}' ../files/number_of_waveforms.txt | gmt psxy -i0,1,2 -Si.25 -R -J \
 -O -K -W.5p -Cseis.cpt -t10 >> $out
 
 # New data
@@ -94,7 +94,7 @@ awk '{print $3, $2, $4}' files/number_of_waveforms.txt | gmt psxy -i0,1,2 -Si.25
 # ------------------------------------------------------------------------------------------------------------------- #
 echo Create legend...
 gmt set FONT_ANNOT_PRIMARY 7
-gmt pslegend <<END -R -J -Dx0.4i/3.1i+w0i/0.0i/TC -C0.07i/0.1i -F+gwhite+pthin -P -O -K >> $out
+gmt pslegend <<END -R -J -Dx.4i/3.1i+w0i/0.0i/TC -C0.07i/0.1i -F+gwhite+pthin -P -O -K >> $out
 G -0.05i
 H 8 Seismic networks
 D0.1i 0.5p
@@ -124,7 +124,38 @@ END
 #gmt pstext -R -J -D0/0.23 -O -K -F+f12p,Helvetica,gray10 -TO -Gwhite -W0.1 >> $out << END
 #$end_lon $end_lat B
 #END
+echo Make basemap...
+#gmt pscoast -W1/0.05 -Dl $proj -R$west/$east/$south/$north -K -O -Y11 -X-0.01 -B0.5WSen -P >> $out
+#gmt psbasemap -R -J -O -K -DjTR+w1.7i+o0.2i/-1.8i+stmp -F+gwhite+p1p >> $out
+#gmt pscoast -Rg -JE10/40/3.0i -Dc -A -Bg -Glightgray -Wthinnest -O -K -X0 -Y-8.5 >> $out
+#gmt coast -Rg -JE10/40/110/4.4i -Bg  -BWsNE -Dc -A10000 -Glightgray -Wthinnest
 
+#gmt makecpt -Chot -T0/200/50 -H > n.cpt
+
+#awk '{print $1, $2, $3/1000}' ../tele_events_sample.txt | gmt psxy -R -J -i0,1,2s0.2 -Sc.05 -Wthinnest -O -K -t5 -Cn.cpt >> $out
+##	# Fancy line
+#gmt psxy -R -J -O -K -B -W2.5p,red >> $out << END
+#0	45
+#20 45
+#20	50
+#0	  50
+#0 45
+#END
+#gmt psscale -DjLM+w5.0c+jRM+o1.5c+e -Cn.cpt -Bxa50+lDEPTH -By+lkm >> $out
+#
+
+
+#gmt psxy << END -R -J -O -W1.,black,- -K>> $out
+#$start_lon $start_lat
+#$end_lon $end_lat
+#END
+#	gmt plot -R0/19/0/25 -Jx1c -B0 -W2p -X-6c -Y-13.5c <<- EOF
+#	3	13
+#	20 13
+#	20 33
+#	3  33
+#	3  13
+#	EOF
 
 # ------------------------------------------------------------------------------------------------------------------- #
 gmt psxy -R -J -T -O >> $out
