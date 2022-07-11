e5dbc8c42cdfe3354611fdcf9732da8bf3972ba6
kmichailos <17120684+kemichai@users.noreply.github.com>
2022-07-11 10:17:27 +0200

    updated si map

--- rfmpy/visualisation/gmt/maps/Supporting_maps/cc_values_map.sh
+++ rfmpy/visualisation/gmt/maps/Supporting_maps/cc_values_map.sh
@@ -33,9 +33,9 @@ gmt pscoast -W1/0.05 -Dl $proj -R$west/$east/$south/$north -K -B5WSen -P -X1 -Y1
 # ------------------------------------------------------------------------------------------------------------------- #
 echo Plot topo....
 #gmt grdimage -R -J /home/kmichall/Desktop/topo/topo.0.20.40.55.3sec.grd -CFrance2.cpt -O -K >> $out
-gmt grdimage -R -J $topodir/ETOPO1_Bed_g_gmt4.grd -Cmy_topo.cpt -O -K >> $out
+#gmt grdimage -R -J $topodir/ETOPO1_Bed_g_gmt4.grd -Cmy_topo.cpt -O -K >> $out
 # ------------------------------------------------------------------------------------------------------------------- #
-gmt pscoast -W1/0.05 -Df -J -R -K -O -P -Sazure1 -N1/0.05p,black -L3.4/49.7/48/200+l+u >> $out
+gmt pscoast -W1/0.05 -Df -J -R -K -O -P -Sazure1 -Ggainsboro -N1/0.05p,black -L3.4/49.7/48/200+l+u >> $out
 
 
 echo Plotting faults and stuff...
@@ -45,12 +45,12 @@ echo Plotting faults and stuff...
 # ---------
 echo Create cpt...
 #gmt makecpt -Cviridis -T40/110/10  > seis.cpt
-gmt makecpt -Chot -T0.85/1/0.01 -I > seis.cpt
+gmt makecpt -Cviridis -T0.88/1/0.01 > seis.cpt
 
 echo Plot scale...
 #gmt psscale -Dx2.5/8.9+o0/0i+w1.2i/0.08i+h+e -R -J -Cmy_topo.cpt -Bx500f250 -Bx+l"Topography (m)" -O -K  >> $out
 #gmt psscale -Dx7.0/8.9+o0/0i+w1.2i/0.08i+h+e -R -J -Cseis.cpt -Bxa400f200 -Bx+l"Number of waveforms" -O -K  >> $out
-gmt psscale -Dx7.0/8.9+o0/0i+w1.2i/0.08i+h+e -R -J -Cseis.cpt -Bxa0.05f.05 -Bx+l"Average cc values" -O -K  >> $out
+gmt psscale -Dx9.0/1.9+o0/0i+w1.1i/0.08i+h+e -R -F+gwhite+p1p -J -Cseis.cpt -Bxa0.05f.05 -Bx+l"Average cc values" -O -K  >> $out
 
 #-Bxaf+l"topography" -By+lkm
 
@@ -79,15 +79,15 @@ awk '{print $1, $2, $3}' ../files/cc_values.txt | gmt psxy -i0,1,2 -Si.2 -R -J \
 # -=================================================================================================================- #
 # ------------------------------------------------------------------------------------------------------------------- #
 echo Create legend...
-gmt set FONT_ANNOT_PRIMARY 7
-gmt pslegend <<END -R -J -Dx3.5i/0.3i+w0i/0.0i/TC -C0.07i/0.1i -F+gwhite+pthin -P -O -K --FONT_ANNOT_PRIMARY=5p >> $out
-G -0.05i
-H 7 Seismic networks
-D0.1i 0.5p
-G .04i
-S .04i i .11i red 0.3p 0.18i AASN, EASI, CIFALPS, PACASE
-G .05i
-END
+#gmt set FONT_ANNOT_PRIMARY 7
+#gmt pslegend <<END -R -J -Dx3.5i/0.3i+w0i/0.0i/TC -C0.07i/0.1i -F+gwhite+pthin -P -O -K --FONT_ANNOT_PRIMARY=5p >> $out
+#G -0.05i
+#H 7 Seismic networks
+#D0.1i 0.5p
+#G .04i
+#S .04i i .11i red 0.3p 0.18i AASN, EASI, CIFALPS, PACASE
+#G .05i
+#END
 
 #echo Plot cross section lines
 #start_lon='8'
