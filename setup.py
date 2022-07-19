8050c511f495b113e477729c0adcc5682297e382
kmichailos <17120684+kemichai@users.noreply.github.com>
2022-07-19 12:50:31 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ init file for base package.
 """
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="rfmpy",
     version="0.0.1",
     author="Konstantinos Michailos",
     author_email="konstantinos.michailos@gmail.com",
-    description="A small set of codes for calculating receiver functions and time to depth migration.",
+    description="Set of codes for calculating receiver functions and time to depth migration.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="",
     classifiers=[
         "Programming Language :: Python :: 3",
