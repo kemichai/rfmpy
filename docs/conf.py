43dec615a7b00718a7b55eed6c1eec9a9f80dd4b
kemichai <mkostas714@gmail.com>
2023-02-03 15:51:55 +0100

    Add mini logo in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# html_logo = 'img/QMlogoBig_rounded.png'
+html_logo = 'images/RFM_logo.png'
 
 # -- Options for HTML output -------------------------------------------------
 
