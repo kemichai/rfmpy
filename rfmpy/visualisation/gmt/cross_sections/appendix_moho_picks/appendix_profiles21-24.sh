6e90ed2d9f1637179bd4bd2866b5cf7392274fd0
kemichai <mkostas714@gmail.com>
2023-02-27 17:22:27 +0100

    Update cc files after re-running RFs This is after I fixed the filter for STA/LTA qc

--- rfmpy/visualisation/gmt/cross_sections/appendix_moho_picks/appendix_profiles21-24.sh
+++ rfmpy/visualisation/gmt/cross_sections/appendix_moho_picks/appendix_profiles21-24.sh
@@ -21,8 +21,8 @@ gmt set MAP_FRAME_TYPE plain
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_24.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 24"
 #gmt psscale -Dx23./-.4+o0/0i+w1.5i/0.1i+h+e -Cpol_vik.cpt -Baf -Bx+l"Relative amplitude (%)"
-awk '{print($4, 6370-$3)}' pick_files/picks_cc24.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black -l"Moho picks"
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc24.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black -l"Unc. Moho picks"
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_24.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black -l"Moho picks"
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_24.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black -l"Unc. Moho picks"
 gmt legend -DjTR+o2.c -F+pthick+gwhite --FONT_ANNOT_PRIMARY=11p
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
@@ -30,22 +30,22 @@ gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 #
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_23.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 23" -Yh+0.2c
-awk '{print($4, 6370-$3)}' pick_files/picks_cc23.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc23.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_23.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_23.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 #
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_22.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 22" -Yh+0.2c
-awk '{print($4, 6370-$3)}' pick_files/picks_cc22.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc22.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_22.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_22.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_21.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 21" -Yh+0.2c
-awk '{print($4, 6370-$3)}' pick_files/picks_cc21.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc21.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_21.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_21.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 
--- rfmpy/visualisation/gmt/cross_sections/appendix_moho_picks/appendix_profiles25-28.sh
+++ rfmpy/visualisation/gmt/cross_sections/appendix_moho_picks/appendix_profiles25-28.sh
@@ -21,8 +21,8 @@ gmt set MAP_FRAME_TYPE plain
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_28.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 28"
 #gmt psscale -Dx23./-.4+o0/0i+w1.5i/0.1i+h+e -Cpol_vik.cpt -Baf -Bx+l"Relative amplitude (%)"
-awk '{print($4, 6370-$3)}' pick_files/picks_cc28.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black -l"Moho picks"
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc28.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black -l"Unc. Moho picks"
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_28.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black -l"Moho picks"
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_28.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black -l"Unc. Moho picks"
 gmt legend -DjTR+o2.c -F+pthick+gwhite --FONT_ANNOT_PRIMARY=11p
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
@@ -30,22 +30,22 @@ gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 #
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_27.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 27" -Yh+0.2c
-awk '{print($4, 6370-$3)}' pick_files/picks_cc27.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc27.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_27.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_27.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 #
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_26.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 26" -Yh+0.2c
-awk '{print($4, 6370-$3)}' pick_files/picks_cc26.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc26.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_26.txt| gmt psxy -Gwhite -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_26.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 
 awk '{print $1, 6370-$2, $3}' cc_files/Cross-section_25.txt| gmt xyz2grd -R0/15.4/6290/6370 -I2.m/2k -Gt_.nc -Vl
 gmt grdview t_.nc -JPa50/7z -T+s0.01p,gray -By10+l"Depth (km)" -Bya5f5 -Bxa1f0.5+l"Distance (km)" -Cpol_vik.cpt -R0/15.3/6290/6370 -BWsNE+t"Cross section 25" -Yh+0.2c
-awk '{print($4, 6370-$3)}' pick_files/picks_cc25.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black
-awk '{print($4, 6370-$3)}' pick_files/unc_picks_cc25.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/moho_depths_Cross-section_25.txt | gmt psxy -Gwhite -Sd.25 -W1.2p,black
+awk '{print($4, 6370-$3)}' pick_files/unc_moho_depths_Cross-section_25.txt | gmt psxy -Gdimgrey -Sd.25 -W1.2p,black
 gmt text -Dj15p -F+cLT+jTL+f17p+t"E"
 gmt text -Dj20p -F+cRT+jTR+f17p+t"W"
 
