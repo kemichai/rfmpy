6b4a59c5d794ec84810d6f30c63c6e28a1ebdab2
kemichai <mkostas714@gmail.com>
2024-01-11 17:11:06 +0100

    Working on documentation

--- docs/conf.py
+++ docs/conf.py
@@ -47,18 +47,18 @@ extensions = [
 ]
 
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = "index"
+# master_doc = "index"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# exclude_patterns = []
 
 html_logo = 'images/rfmpy_logo_mini.png'
 
@@ -70,7 +70,7 @@ html_logo = 'images/rfmpy_logo_mini.png'
 # html_theme = 'sphinx_rtd_theme'
 
 # generate automatically stubs
-autosummary_generate = glob.glob("submodules" + os.sep + "*.rst")
+# autosummary_generate = glob.glob("submodules" + os.sep + "*.rst")
 
 # # Add any paths that contain custom static files (such as style sheets) here,
 # # relative to this directory. They are copied after the builtin static files,
