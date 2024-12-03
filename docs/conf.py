edc19126c30b03504e202ca9bde7281fe74d79b2
kemichai <mkostas714@gmail.com>
2024-12-03 15:58:45 +1100

    Update stuff

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,10 @@ extensions = [
 
 # The master toctree document.
 # master_doc = "index"
+# Need to tell the autoapi that our source code is one level up
+autoapi_type = "python"
+autoapi_dirs = ["../rfmpy"]
+autoapi_add_toctree_entry = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -64,33 +68,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 # exclude_patterns = []
-
 html_logo = 'images/rfmpy_logo_mini.png'
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'sphinx_rtd_theme'
-
-# generate automatically stubs
-# autosummary_generate = glob.glob("submodules" + os.sep + "*.rst")
-
-# # Add any paths that contain custom static files (such as style sheets) here,
-# # relative to this directory. They are copied after the builtin static files,
-# # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = []
-
-
-
-# Need to tell the autoapi that our source code is one level up
-autoapi_type = "python"
-autoapi_dirs = ["../rfmpy"]
-autoapi_add_toctree_entry = True
-
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -125,18 +103,7 @@ pygments_style = None
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-# html_static_path = []
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
