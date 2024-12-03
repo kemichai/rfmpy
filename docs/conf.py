a259e7d9f8e23d1a0350b20168ec9ced3a726521
kemichai <mkostas714@gmail.com>
2024-12-03 16:30:53 +1100

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -224,7 +224,11 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+# intersphinx_mapping = {'https://docs.python.org/': None}
+
+intersphinx_mapping = {
+    'https://docs.python.org/3/': ('https://docs.python.org/3/objects.inv', None),
+}
 
 # -- Options for todo extension ----------------------------------------------
 
